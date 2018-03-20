666ddd6ff8f42adfc797b571cbf85f19fd78be3b
Sharmina Akhtar <sakht318@gmail.com>
2018-03-20 09:29:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,7 @@ class ApplicationController < Sinatra::Base
     if params[:username] == "" || params[:password] == ""
        redirect "/failure"
     else
+      user
        redirect "/login"
     end
 
