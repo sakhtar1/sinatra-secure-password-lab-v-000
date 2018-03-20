4c6efc6ba24b195873eb88a8a26f6bc8416355d1
Sharmina Akhtar <sakht318@gmail.com>
2018-03-20 09:07:41 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320082033) do
+ActiveRecord::Schema.define(version: 20180320090649) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
